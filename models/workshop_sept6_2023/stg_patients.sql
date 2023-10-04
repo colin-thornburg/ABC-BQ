914514770a45a702b269c8a6af0998d8550ce111
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2023-10-04 08:22:59 +0000

    more cleanup

--- models/workshop_sept6_2023/stg_patients.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    Patient_ID as patient_id,
-    Patient_Name as patient_name,
-    Date_of_Birth as date_of_birth,
-    loaded_at as loaded_at_timestamp
-FROM 
-    {{ source('health_claims','patients') }}
